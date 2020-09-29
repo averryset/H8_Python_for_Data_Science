3462f90035e9b70e0425d07830b45dbc0af32f04
Ahmad Verry Setiawan <avs.ahmadverry@gmail.com>
2020-09-29 22:00:00 +0700

    Day 3 Function, Modules

---
+++ model.py
@@ -0,0 +1,13 @@
+def say_hello_student():
+    """Hello Student"""
+    students = ["Ahmad", "Verry", "Setiawan"]
+    number = [1,2,3,4]
+    for i in range(len(number)):
+        print(i)
+    return students
+
+def compute_paramete(x, y, z):
+    """Aritmatika"""
+    z = x + y
+    a = z * z
+    return a
